f41691ef360b30d1d757d42b588f420eadbb9a07
Robin Verleun <verleun.ro@gmail.com>
2017-06-05 17:04:51 -0600

    Voting app started

---
+++ voting-server/test/immutable_spec.js
@@ -0,0 +1,13 @@
+import {expect} from 'chai';
+
+describe('immutability', () => {
+  
+  describe('a number', () => {
+    
+    function increment(currentState) {
+      return currentState + 1;
+    }
+    
+    it('is immutable ')
+  })
+})
